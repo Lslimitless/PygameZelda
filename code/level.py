039b29f01cd464133c5c6d4df37579c9739d760b
YongDeok <81160857+Lslimitless@users.noreply.github.com>
2023-04-12 06:46:49 +0000

    hitbox

--- code/level.py
+++ code/level.py
@@ -3,7 +3,9 @@ from settings import *
 from tile import Tile
 from player import Player
 
+
 class Level:
+
     def __init__(self):
 
         self.display_surface = pygame.display.get_surface()
@@ -14,7 +16,7 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        
+
         for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
@@ -22,16 +24,19 @@ class Level:
                 if col == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
                 if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
-    
+                    self.player = Player((x, y), [self.visible_sprites],
+                                         self.obstacle_sprites)
+
     def run(self):
-        
+
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
+
 class YSortCameraGroup(pygame.sprite.Group):
+
     def __init__(self):
-        
+
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
@@ -43,6 +48,7 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        for sprite in self.sprites():
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
--- code/player.py
+++ code/player.py
@@ -7,6 +7,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -20)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
@@ -32,11 +33,11 @@ class Player(pygame.sprite.Sprite):
         if self.direction.magnitude() != 0: # magnitude() : 피타고라스의 정리를 이용한 벡터의 길이를 반환함 sqrt(x^2 + y^2) -> r^2 = x^2 + y^2
             self.direction = self.direction.normalize() # normalize() : 방향이 같은 길이가 1인 벡터를 반환함
 
-        self.rect.x += self.direction.x * speed
+        self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
-        self.rect.y += self.direction.y * speed
+        self.hitbox.y += self.direction.y * speed
         self.collision('virtical')
-        # self.rect.center += self.direction * speed
+        self.rect.center = self.hitbox.center
         
         # pygame.rect 는 실수를 표현할 수 없기 때문에 소수 대입 시 반올림으로 적용함
         # 예전에는 버림 처리 하였기에 좌,우 그리고 상,하 각각 이동거리가 달랐음
@@ -45,19 +46,19 @@ class Player(pygame.sprite.Sprite):
     def collision(self, direction):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x > 0: # 오른쪽으로 이동
-                        self.rect.right = sprite.rect.left
+                        self.hitbox.right = sprite.hitbox.left
                     if self.direction.x < 0: # 왼쪽으로 이동
-                        self.rect.left = sprite.rect.right
+                        self.hitbox.left = sprite.hitbox.right
 
         if direction == 'virtical':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.y > 0: # 아래쪽으로 이동
-                        self.rect.bottom = sprite.rect.top
+                        self.hitbox.bottom = sprite.hitbox.top
                     if self.direction.y < 0: # 위쪽으로 이동
-                        self.rect.top = sprite.rect.bottom
+                        self.hitbox.top = sprite.hitbox.bottom
 
     def update(self):
         self.input()
